99d2dca74f69736cf6b89b2ab6089f76e190f357
AliaksandrRauko <ravkoaal@mail.ru>
2021-12-28 12:51:57 +0300

    setup main

---
+++ HomeWork_2_7_1/Services/DataManager.swift
@@ -0,0 +1,29 @@
+//
+//  DataManager.swift
+//  HomeWork_2_7_1
+//
+//  Created by Aliaksandr Rauko on 28.12.21.
+//
+
+class DataManager {
+    
+    static let Shared = DataManager()
+    
+    let names = ["Jhon", "Aaron" , "Tim", "Ted",
+                "Steven", "Sharon","Nicola",
+                "Allan", "Bruce", "Carl"]
+    
+    let surnames = ["Smith", "Dow" , "Black", "Green",
+                "Kolt", "Magnum","Werty",
+                "Williams", "Label", "Hertuo"]
+    
+    let emails = ["www.Smith.com", "www.Dow.com" , "www.vBlack.com", "www.Green.com",
+                "www.Kolt.com", "www.Magnum.com","www.Werty.com",
+                "www.Williams.com", "www.Label.com", "www.Hertuo.com"]
+    
+    let phones = ["1", "2" , "3", "4",
+                "5", "6","7",
+                "8", "9", "10"]
+    
+}
+
